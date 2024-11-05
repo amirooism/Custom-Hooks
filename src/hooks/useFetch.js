69993e1697060c0fd7df2cc129e52ec7bea293b1
amiro hatami <amirohatami@amiros-Laptop.local>
2024-11-05 11:43:14 +0330

    i create custom hook name useFetch, and add all my fetchUserPlaces to it with the useEffect and all the state, and make it resuable, now my app.jsx is more learner but all other fuction left yet

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(fetchFn , initialvalue) {
+  //function start with "use" treated as hooks :)
+
+  const [fetchedData, setFetchedData] = useState(initialvalue);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+  return {
+    isFetching,
+    fetchedData,
+    error,
+  };
+}
