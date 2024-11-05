204a2e72ce59bd74749e89edff2f5c7783285ff9
amiro hatami <amirohatami@amiros-Laptop.local>
2024-11-05 13:48:12 +0330

    Finish the app by creating fetchSortedPlaces function in AvalPlace jsx component and adding the user location to the availbaleplaces we have, and use Promis method to create a promes for the fetchFn which wait for the promis :)

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,34 +1,38 @@
-import { useState, useEffect } from "react";
-
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
-import { useFetch } from '../hooks/useFetch.js'
+import { useFetch } from "../hooks/useFetch.js";
 
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
 
-// navigator.geolocation.getCurrentPosition((position) => {
-//   const sortedPlaces = sortPlacesByDistance(
-//     places,
-//     position.coords.latitude,
-//     position.coords.longitude
-//   );
-//   setAvailablePlaces(sortedPlaces);
-//   setIsFetching(false);
-// });
-
+  // we have to make sure we return a promise here after we got the current position
+  //this promis object is a feature that is build in to the browsers, ths object take an functin as argumnet and then in
+  // that func you get two pamaeters passed in by the browser, A Resolve and Reject paramete, and in that function body
+  //we should add the code that we get the user location
+  return new Promise((Resolve, Reject) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+        // we put this function to a promis because useFetch expect a promis becasue we await a promis befor fetchFn :) "i hope you get it"
+        // now we should make sure this function Resolve to a value,
+        //because the fechFn expect a value
+        //here we should  call resolve and pas the sortedValue as Resovle Value to Resolve
+      );
+      Resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
-
-
   const {
     isFetching,
     error,
     fetchedData: availablePlaces,
-    setFetchedData: setAvailablePlaces,
-  } = useFetch(fetchAvailablePlaces, []);
-
-  
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
